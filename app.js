784a8837b090b156a8e7f9bc0bb66fc433f25ef2
nuhakhangr8 <nuhakhangr8@gmail.com>
2021-08-24 09:36:27 +0530

    feat: outputDiv

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var btnTranslate=document.querySelector("#btn-translate")
 var inputTxt=document.querySelector("#txt-input")
+var outputDiv=document.querySelector("#output")
 
+var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 function clickHandler() {
 
-    var outputtext=inputTxt.value;
-        console.log(outputtext);
+    fetch(url)
+    .then(response=>response.json())
+    .then(json=>console.log(json))
+     //outputDiv.innerText="thouest thhees thuus "+inputTxt.value;
+       // console.log(outputDiv.innerText);
         console.log("clicked!");
 }
 
